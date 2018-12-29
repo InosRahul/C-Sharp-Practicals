74c0a9f01f4d43a30a14e252aa80623b7c3a4a8e
InosRahul <rahulsoni2225@gmail.com>
2018-12-29 21:54:04 +0530

    Matrix pattern done

---
+++ star matrix pattern/star matrix pattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace star_matrix_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_of_lines;
+            Console.WriteLine("Enter number of lines for pattern:");
+            number_of_lines = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < number_of_lines; i++) {
+                for (int j = 0; j < number_of_lines; j++) {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
