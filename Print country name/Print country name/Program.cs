6a4dd75c43540e0962077043171456bc2d09b40a
InosRahul <rahulsoni2225@gmail.com>
2018-12-29 21:43:58 +0530

    files added

---
+++ Print country name/Print country name/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_country_name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "";
+            String country = "";
+            Console.WriteLine("Enter name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Country:");
+            country = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " from " + country);
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ foreach/foreach/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int[] b = new int[10];
+            for (int j = 0; j < 10; j++) {
+                b[j] = Convert.ToInt16(Console.ReadLine());
+            }
+                foreach (int x in b)
+                {
+                    Console.WriteLine("{0}", x);
+                }
+            //Console.ReadKey();
+            foreach(int i in a){
+                Console.WriteLine("{0}", i);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
